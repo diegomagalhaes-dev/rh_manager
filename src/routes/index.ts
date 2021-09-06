0843c64cca2a8953cacac982573bc47d1f410850
diegomagalhaes-dev <digengup@gmail.com>
2021-09-05 22:43:24 -0300

    :zap: Insert validate routes in routes index

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 
 import registerRoutes from './register.routes';
+import validateRoutes from './validate.routes';
 
 const routes = Router();
 
 routes.use('/register', registerRoutes);
 export default routes;
+routes.use('/registers', validateRoutes);
